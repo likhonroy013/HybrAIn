6429d5e3a11dd5fe725ffbf935c4faaf317a37e2
Likhon Roy <likhon.roy@getnexar.com>
2026-02-22 21:30:29 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HyBrainPrototype from './HyBrainPrototype.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HyBrainPrototype />
+  </React.StrictMode>
+)
